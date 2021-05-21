14a2fdd83b3399f0b176caf2cde33f03ea281155
Andrew Lu <lvmzcn@gmail.com>
2021-05-21 17:42:00 -0400

    styling earthquakes map

--- Earthquakes_past7days/js/logicStep1.js
+++ Earthquakes_past7days/js/logicStep1.js
@@ -88,9 +88,10 @@ L.control.layers(baseMaps).addTo(map);
 
 // Create a style for the lines.
 // let myStyle = {
-//     color: "blue",
-//     fillColor: "yellow",
-//     weight: 1
+//     color: "#ffffa1",
+//     fillColor: "purple",
+//     weight: 2
+//     }
 
 // }
 // torontoHoods = "torontoNeighborhoods.json";
@@ -106,6 +107,32 @@ L.control.layers(baseMaps).addTo(map);
 // })
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  console.log(data);
+  L.geoJSON(data,{
+      pointToLayer: function(feature,latlng){
+          return L.circleMarker(latlng)
+      },
+      style: styleInfo
+  }).addTo(map);
 });
+
+function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
